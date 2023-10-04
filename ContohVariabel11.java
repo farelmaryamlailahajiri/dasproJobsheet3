3b2c44cf49fa93d90db935acaacdd0234b23bf8a
Farel <farelhajiri2@gmail.com>
2023-10-04 19:59:02 +0700

    daspro-jobsheet3 saya

---
+++ ContohVariabel11.java
@@ -0,0 +1,14 @@
+public class ContohVariabel11 {
+    public static void main (String[] args){
+            String salahSatuHobySayaAdalah = "Membaca dan menulis buku";
+        boolean isPandai = true;
+        char jenisKelamin = 'P';
+        byte _umurSayaSekarang = 18;
+        double $ipk = 3.24, tinggi = 1.65;
+        System.out.println("salah satu hoby saya adalah: " + salahSatuHobySayaAdalah);
+        System.out.println("Apakah pandai?" + isPandai);
+        System.out.println("Jenis kelamin: " + jenisKelamin);
+        System.out.println("Umurku saat ini: " + _umurSayaSekarang);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+    }
+}
